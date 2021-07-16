ed97dc4896bb6c57ec1cd3bab956d6dac8dbf6d6
Bukola Omojowo <bomojowo@gmail.com>
2021-07-16 06:45:06 +0100

    Formatted files

--- src/components/Search.tsx
+++ src/components/Search.tsx
@@ -1,14 +1,11 @@
-import "./Search.css"
+import "./Search.css";
 
-function Search():JSX.Element {
-    return (
-        <div>
-            <input
-            className="search-bar"
-            placeholder="Search"
-            />
-        </div>
-    )
+function Search(): JSX.Element {
+  return (
+    <div>
+      <input className="search-bar" placeholder="Search" />
+    </div>
+  );
 }
 
-export default Search
+export default Search;
