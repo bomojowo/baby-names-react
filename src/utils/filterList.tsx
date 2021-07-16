cf6f81f91cab74ce8bf67612554de9ad36c97b70
Bukola Omojowo <bomojowo@gmail.com>
2021-07-16 08:32:02 +0100

    Added filterList util function

---
+++ src/utils/filterList.tsx
@@ -0,0 +1,30 @@
+import babyNames from "../data/babyNamesData.json"
+
+interface filterListProps{
+    search: string
+}
+
+interface babyNameProps {
+id: number;
+name: string;
+sex: string;
+}
+
+
+
+function filterList({search}: filterListProps): babyNameProps[] {
+    const filteredList = babyNames.filter((babyname: babyNameProps) => {
+        const emptyString = ""
+        const nameContainsSearch = babyname.name.toLowerCase().includes(search.toLowerCase())
+        
+        if(emptyString || nameContainsSearch){
+            return true;
+        }else {
+            return false
+        }
+    })
+    return filteredList
+
+}
+
+export default filterList
