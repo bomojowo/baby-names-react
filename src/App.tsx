25008d531db5bee6b606bb7db526f929de93e03e
Bukola Omojowo <bomojowo@gmail.com>
2021-07-16 08:34:24 +0100

    Passed down props through search ccomponent

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import Search from "./components/Search";
 import DisplayNames from "./components/DisplayNames";
+
 
 import "./App.css";
 
 function App() {
+  const [search, setSearch] =useState<string>("")
+
   return (
     <div className="App">
-      <Search />
-      <DisplayNames />
+      <Search setSearch={setSearch} />
+      <DisplayNames search={search} />
     </div>
   );
 }
