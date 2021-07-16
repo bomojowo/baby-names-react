43adb651405c016f16868aa68278124b0171e35f
Bukola Omojowo <bomojowo@gmail.com>
2021-07-16 06:44:39 +0100

    Added DisplayNames and search components

---
+++ src/components/DisplayNames.tsx
@@ -0,0 +1,22 @@
+import babyNames from "../data/babyNamesData.json"
+import "./DisplayNames.css"
+interface IName {
+    id: number,
+    name: string,
+    sex: string
+}
+function DisplayNames(): JSX.Element {
+    return(
+        <div>
+            <div className="names-container">
+            {babyNames.map((babyName: IName) => (
+                <div key={babyName.id} className="each-name">
+                    {babyName.name} ({babyName.sex})
+                </div>
+            ))}
+            </div>
+        </div>
+    )
+}
+
+export default DisplayNames
---
+++ src/components/Search.tsx
@@ -0,0 +1,14 @@
+import "./Search.css"
+
+function Search():JSX.Element {
+    return (
+        <div>
+            <input
+            className="search-bar"
+            placeholder="Search"
+            />
+        </div>
+    )
+}
+
+export default Search
